1473b92482bcd11f13833fa6b48bc4a85210926c
Nataliia <nstasiukova@gmail.com>
2023-01-24 15:03:37 +0200

    Classwork-60

---
+++ src/main/java/telran/test/AccountingManagmentTestApplication.java
@@ -0,0 +1,15 @@
+package telran.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = {"telran"})
+public class AccountingManagmentTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountingManagmentTestApplication.class, args);
+	}
+
+}
